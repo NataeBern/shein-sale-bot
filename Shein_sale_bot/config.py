bba820792d0d802173416d7d4dd73c6bdaa4612b
Maksimova Anastasia <nata.e.bern@gmail.com>
2023-10-24 19:05:28 +0300

    Delete Shein_sale_bot directory

--- Shein_sale_bot/config.py
+++
@@ -1,7 +0,0 @@
-token_API = '6330055172:AAGRAka4wVRKi5cHtLeqMr86oAvM8-I0l8I'
-
-chanel_ID = '@sheininsale'
-
-ADMIN = '6662648924'
-
-bot_URL = '@Shein_sale_bot'
--- Shein_sale_bot/keyboards.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-kb.add(KeyboardButton('Жми сюда!'))
